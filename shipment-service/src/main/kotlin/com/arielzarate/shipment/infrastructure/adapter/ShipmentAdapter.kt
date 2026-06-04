dc648af6b8354f55fcb4dac2799374f610da78b4
ariel Zarate <arieltecnico@gmail.com>
2026-06-03 22:29:46 -0300

    refactor

---
+++ shipment-service/src/main/kotlin/com/arielzarate/shipment/infrastructure/adapter/ShipmentAdapter.kt
@@ -0,0 +1,28 @@
+package com.arielzarate.shipment.infrastructure.adapter
+
+import com.arielzarate.shipment.domain.model.Shipment
+import com.arielzarate.shipment.domain.ports.out.ShipmentProvider
+import com.arielzarate.shipment.infrastructure.persistence.mapper.ShipmentMapper
+import com.arielzarate.shipment.infrastructure.persistence.repository.ShipmentRepository
+import org.springframework.stereotype.Component
+import java.util.UUID
+
+@Component
+class ShipmentAdapter(
+    private val repository: ShipmentRepository, private val mapper: ShipmentMapper
+) : ShipmentProvider {
+
+    override fun findAll(): List<Shipment> {
+        return repository.findAll().map { mapper.toDomain(it) }
+    }
+
+    override fun findByOrderId(orderId: String): Shipment? {
+        return repository.findById(UUID.fromString(orderId)).map (mapper::toDomain).orElse(null)
+    }
+
+    override fun save(shipment: Shipment): Shipment {
+        val entity = mapper.toEntity(shipment)
+        val saved = repository.save(entity)
+        return mapper.toDomain(saved)
+    }
+}
